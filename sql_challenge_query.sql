2e40d0782fec97d0c623a222704d756baa806204
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-08-28 21:04:47 -0400

    Working of query 3 and 4, missing a piece to the employee part of the  query. Still working.

--- sql_challenge_query.sql
+++ sql_challenge_query.sql
@@ -1,9 +1,9 @@
 -- SQL-Challenge Query Code
 -- Objectives: 
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
 
 
--- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
@@ -21,4 +21,9 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 ---------------------------------------------------------------------------------
--- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+-- ****3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT departments.dept_no, departments.dept_name, dept_man.emp_no, dept_man.dept_no
+FROM departments
+JOIN dept_man ON dept_man.dept_no=departments.dept_no
+---------------------------------------------------------------------------------
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
