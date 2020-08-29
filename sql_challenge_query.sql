12580aee9e47c7a74a9e089e7c81bb2c829d76ba
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-08-29 15:11:10 -0400

    Have a few bugs in the last half of the queries, but I have very solid skeletons. Need to iron out the bugs.

--- sql_challenge_query.sql
+++ sql_challenge_query.sql
@@ -1,13 +1,11 @@
 -- SQL-Challenge Query Code
 -- Objectives: 
--- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+-- 5. *** List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+-- 6. *** List all employees in the Sales department, including their employee number, last name, first name, and department name.
+-- 7. *** List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- 8. *** In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
 
-
-
--- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
--- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
--- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
 -- 1. List the following details of each employee: 
 --employee number, last name, first name, sex, and salary.
@@ -36,5 +34,31 @@ FROM employees
 JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
 JOIN departments ON departments.dept_no=dept_emp.dept_no;
 ---------------------------------------------------------------------------------
--- 5. List first name, last name, and sex for employees 
+-- 5. *** List first name, last name, and sex for employees 
 --whose first name is "Hercules" and last names begin with "B."
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules' and last_name like 'B'
+---------------------------------------------------------------------------------
+-- 6. *** List all employees in the Sales department, 
+-- including their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees, departments
+where dept_name = 'Sales department'
+join employees on employees.emp_no=dept_emp.emp_no;
+JOIN departments on departments.dept_no=dept_emp.dept_no
+
+---------------------------------------------------------------------------------
+-- 7. *** List all employees in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees,departments
+where dept_name = 'Sales department' or dept_name = 'Development department'
+join employees on employees.emp_no=dept_emp.emp_no
+join dept_emp on dept_emp.dept_no=departments.dept_no
+---------------------------------------------------------------------------------
+-- 8. *** In descending order, list the frequency count of employee last names, 
+-- i.e., how many employees share each last name.
+select count(last_name)
+from employees
+ORDER BY employees.last_name DESC;
